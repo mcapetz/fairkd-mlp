061275c4e44d631fc2fc88df28b5284ffcd29f8d
Margaret Capetz <57432165+mcapetz@users.noreply.github.com>
2025-07-29 16:08:56 -0700

    Update prepare_env.sh

--- prepare_env.sh
+++ prepare_env.sh
@@ -1,3 +1,7 @@
+# This code in part draws from: https://github.com/snap-research/graphless-neural-networks
+# Please see the license here: https://github.com/snap-research/graphless-neural-networks/blob/main/LICENSE
+# Changes were made to the original code.
+
 #!/bin/bash
 
 conda create -y -n glnn python=3.6.9
